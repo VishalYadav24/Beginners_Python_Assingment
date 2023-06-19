4f993b491d903ab44f09a8ddcf6e9f22c998bfe8
VishalYadav24 <112162266+VishalYadav24@users.noreply.github.com>
2023-06-19 08:45:31 +0530

    added doc string

--- Assingment 02/main.py
+++ Assingment 02/main.py
@@ -1,14 +1,24 @@
 import argparse
 import re
-def sort_string(text:str):
+def sort_alphanumeric_string(alphanumeric_text:str):
+ """ Sort a given alphanumeric string in to list of strings following below pattern
+       - small case alphabets comes first
+       - large case alphabets comes second
+       - odd numbers come third
+       - even numbers come at last
+
+     Arguments: alphanumeric_text - string to sort given by user
+
+     Returns:
+       List of sorted strings 
+ """
  small_case_list = []
  upper_case_list = []
  numeric_case_list = []
  regex_small_case = r'[a-z]'
  regex_upper_case = r'[A-Z]'
  regex_numeric_case = r'[0-9]'
- for characters in text:
-   
+ for characters in alphanumeric_text:
     if re.findall(regex_small_case,characters):
      small_case_list.append(characters)
     if re.findall(regex_upper_case,characters):
@@ -31,6 +41,6 @@ def sort_string(text:str):
 
 if __name__ == "__main__":
  parser = argparse.ArgumentParser(description="Sort a provided alphanumeric string")
- parser.add_argument('text',metavar='alphanumeric string',type=str,help='enter text made of alphanumeric characters')
- args:str = parser.parse_args().text
- print(sort_string(args))
+ parser.add_argument('alphanumeric_text',metavar='alphanumeric string',type=str,help='enter alphanumeric_text made of alphanumeric characters')
+ args:str = parser.parse_args().alphanumeric_text
+ print(sort_alphanumeric_string(args))
