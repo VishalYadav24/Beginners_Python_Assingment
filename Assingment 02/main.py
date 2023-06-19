816652fbe92355d8589f87a7aa6ad599e7769dcf
VishalYadav24 <112162266+VishalYadav24@users.noreply.github.com>
2023-06-19 17:24:42 +0530

    added doc string

--- Assingment 02/main.py
+++ Assingment 02/main.py
@@ -2,7 +2,18 @@ import argparse
 import re
 
 
-def sort_alphanumeric_string(text: str):
+def sort_alphanumeric_string(alphanumeric_text: str):
+    """Sort a given alphanumeric string in to list of strings following below pattern
+        - small case alphabets comes first
+        - large case alphabets comes second
+        - odd numbers come third
+        - even numbers come at last
+
+    Arguments: alphanumeric_text - string to sort given by user
+
+    Returns:
+        List of sorted strings
+"""
     small_case_list = []
     upper_case_list = []
     odd_numbers_list = []
@@ -10,7 +21,7 @@ def sort_alphanumeric_string(text: str):
 
     regex_pattern = r"[a-z]|[A-Z]|[0-9]"
 
-    for characters in text:
+    for characters in alphanumeric_text:
         match = re.search(regex_pattern, characters)
         if match:
             group = match.group()
