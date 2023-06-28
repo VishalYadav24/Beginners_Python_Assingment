4cfc3ccb94ee1d64f4dbe2be33da8ca8c989877f
VishalYadav24 <112162266+VishalYadav24@users.noreply.github.com>
2023-06-28 11:24:14 +0530

    linting and doc string

--- Intermediate/Average waiting time/main.py
+++ Intermediate/Average waiting time/main.py
@@ -1,10 +1,22 @@
 import argparse
 
 
-def calculate_least_waiting_time(order_count, order_details):
-    temp = []
-    current_time = 0
-    total_waiting_time = 0
+def calculate_least_waiting_time(order_count: str, order_details: str) -> int:
+    """
+    Calculates the least average waiting time for a given pizza order to be served to a customer.
+    
+    Arguments:
+       order_count: str -> total number of order to be served i.e length of the order_details list
+       order_details: str -> list of order details (customer arrival time, pizza cooking time)
+
+    Returns:
+        Least Average Waiting Time (int)    
+
+    """
+    temp: list = []
+    current_time: int = 0
+    total_waiting_time: int = 0
+    # checking if user has provided order_details upto order_count
     if len(order_details) != 2 * int(order_count):
         raise ValueError("Please provide input equal to order_count")
     else:
@@ -14,40 +26,32 @@ def calculate_least_waiting_time(order_count, order_details):
         ]
         # sort the list by customer arrival time
         converted_list.sort(key=lambda x: x[0])
-        print(converted_list)
+        # moving the order_details element which has less waiting time first
         for i in range(0, len(converted_list) - 1):
             current_arrival_time = converted_list[i][0]
             next_arrival_time = converted_list[i + 1][0]
             current_cooking_time = converted_list[i][1]
             next_cooking_time = converted_list[i + 1][1]
-            print(
-                "ho",
-                current_arrival_time,
-                current_cooking_time,
-                next_arrival_time,
-                next_cooking_time,
-            )
+
             if (current_time - current_arrival_time + current_cooking_time) < (
                 current_time - next_arrival_time + next_cooking_time
             ):
                 pass
             else:
+                # swapping positions
                 temp = converted_list[i]
-                converted_list[i] = converted_list[i+1]
-                converted_list[i+1] = temp
+                converted_list[i] = converted_list[i + 1]
+                converted_list[i + 1] = temp
 
-        print("hpoo", converted_list)
+        # computing total waiting time and average waiting time
         for arrival_time, cooking_time in converted_list:
             if current_time > arrival_time:
                 total_waiting_time += current_time - arrival_time + cooking_time
-                print("1", current_time, arrival_time, total_waiting_time)
             else:
                 current_time = arrival_time
                 total_waiting_time += current_time - arrival_time + cooking_time
-                print("2", current_time, arrival_time, total_waiting_time)
             current_time += cooking_time
-            print("3", current_time, arrival_time, total_waiting_time)
-        print(current_time, total_waiting_time // len(converted_list))
+        return (total_waiting_time // len(converted_list))
 
 
 if __name__ == "__main__":
@@ -67,4 +71,4 @@ if __name__ == "__main__":
     )
     order_count: str = parser.parse_args().no_of_orders
     order_details: str = parser.parse_args().customer_arrival_time_and_pizza_making_time
-    calculate_least_waiting_time(order_count, order_details)
+    print("Average waiting time is :",calculate_least_waiting_time(order_count, order_details))
