18c51b43a34ef96a3a4f1251f809ec9dbfe49586
VishalYadav24 <112162266+VishalYadav24@users.noreply.github.com>
2023-06-28 15:56:38 +0530

    test case

--- Intermediate/Average waiting time/test_main.py
+++ Intermediate/Average waiting time/test_main.py
@@ -5,3 +5,5 @@ from main import calculate_least_waiting_time
 def test_calculate_least_waiting_time():
     sample_order_count = 4
     sample_order_details = ['0', '1', '2', '5', '3', '6', '8', '9']
+    response = calculate_least_waiting_time(sample_order_count,sample_order_details)
+    assert response == 7
