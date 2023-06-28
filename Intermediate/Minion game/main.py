3ceace85bd2f4b6aac9054f8944b9dc690c79ae2
VishalYadav24 <112162266+VishalYadav24@users.noreply.github.com>
2023-06-28 10:35:48 +0530

    linting and doc string

--- Intermediate/Minion game/main.py
+++ Intermediate/Minion game/main.py
@@ -2,24 +2,34 @@ import argparse
 import re
 
 
-def minion_game_score_counter(text):
+def minion_game_score_counter(text: str) -> str:
+    """
+    Calculate the minion game score by comparing word formed using vowels and consonants, and return winner
+
+    Arguments:
+        text: string  user provided text (upper case)
+
+    Returns:
+        message: string - containing winner and its score
+    """
     regex_patter = r"[A-Z]*"
     match = re.search(regex_patter, text)
     if len(match.group()) == len(text):
-        consonant_score = 0
-        vowel_score = 0
+        consonant_score: int = 0
+        vowel_score: int = 0
 
         for i in range(len(text)):
             if text[i] in "AIEOU":
+                # slicing the list with current value of i and taking its length = no of word which can be formed
                 vowel_score += len(text[i:])
             else:
                 consonant_score += len(text) - i
         if vowel_score > consonant_score:
-            print(f"Kevin wins with score  of {vowel_score}")
+            return f"Kevin wins with score  of {vowel_score}"
         elif vowel_score == consonant_score:
-            print(f"Draw no one wins with equal score  of {vowel_score}")
+            return f"Draw no one wins with equal score  of {vowel_score}"
         else:
-            print(f"Stuart wins with score  of {consonant_score}")
+            return f"Stuart wins with score  of {consonant_score}"
     else:
         raise ValueError("Please provide only uppercase letters")
 
@@ -35,4 +45,4 @@ if __name__ == "__main__":
         help="Please provide text / string ",
     )
     args: str = parser.parse_args().minion_text
-    minion_game_score_counter(args)
+    print(minion_game_score_counter(args))
