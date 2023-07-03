bdbda1fc47afc92433c3f89c6597e7847a9166cd
VishalYadav24 <112162266+VishalYadav24@users.noreply.github.com>
2023-07-03 12:05:50 +0530

    send email code

--- Beginners/Cron Job/main.py
+++ Beginners/Cron Job/main.py
@@ -3,6 +3,7 @@ import logging
 import schedule
 import time
 import random
+import smtplib
 
 api_url: str = "https://type.fit/api/quotes"
 
@@ -12,6 +13,24 @@ logging.basicConfig(
     filemode="w",
     format="%(asctime)s %(levelname)s :  %(message)s",
 )
+
+
+def send_email(message: str):
+    """
+    Sends an email to the specified person about critical failures in program
+    """
+    try:
+        session = smtplib.SMTP("smtp.gmail.com", 587)
+        # tls
+        session.starttls()
+        # authentication
+        session.login("sender_email_id", "sender_email_password")
+        # sending mail
+        session.sendmail("sender_email_id", "recipients_email", message)
+        # quit
+        session.quit()
+    except Exception as e:
+        logging.critical(f"Error sending mail:{e}")
 
 
 def display_wise_thoughts() -> None:
@@ -28,11 +47,13 @@ def display_wise_thoughts() -> None:
         )
         logging.info(quote)
     except Exception as e:
-        logging.warning(e)
+        logging.critical(e)
+        send_email(e)
+
 
 schedule.every(2).seconds.do(display_wise_thoughts)
 if __name__ == "__main__":
-    counter:int = 0
+    counter: int = 0
     while True:
         schedule.run_pending()
         time.sleep(1)
